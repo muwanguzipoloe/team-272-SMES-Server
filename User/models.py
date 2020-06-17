e842593d0015860208c5ebc79b7f456916dd7ce0
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-06-17 23:18:11 +0300

    User model

--- User/models.py
+++ User/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    role_choices = (
+        ('sme', ('SME')),
+        ('funder', ('Funder')),
+        ('admin', ('admin')),
+    ) 
+    
+    user_role = models.CharField(
+        max_length=32,
+        choices=role_choices,
+        default='',
+    )
+
+
+
+
+
